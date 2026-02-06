3862b681a7b6e87da564330fcd79b929b493e0c4
BeetlesTheHunter <jhon31708@gmail.com>
2026-02-06 19:08:08 +0900

    findStudentの関数を作りました

--- src/App.java
+++ src/App.java
@@ -1,10 +1,21 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class App {
+    private static ArrayList<Student> studentList = new ArrayList<Student>();
+    private static HashMap<String,Student> studentMap = new HashMap<String,Student>();
+
     public static void main(String[] args) throws Exception {
         Login l = new Login();
         l.loginScreen();
     }
 
+    public static void makeStudentMap(){//ロード関数にこれを追加してください。
+        for (int i = 0; i < studentList.size(); i++){
+            studentMap.put(studentList.get(i).getStudentId(), studentList.get(i));
+        }
+    }
     public static Student findStudent(String _ID){
-        return null; //Student のリストからIDで探します。
+        return studentMap.get(_ID); //Student のリストからIDで探します。
     }
 }
