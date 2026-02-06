2986c57e2066fe2fae9075e94defc45ce91fc804
25j016hayashidashinichi <25j016_hayashida_shinichi@edu.kimura.ac.jp>
2026-02-06 14:59:35 +0900

    Teacherクラス作った。

---
+++ src/Teacher.java
@@ -0,0 +1,21 @@
+public class Teacher {
+    
+    private String teacherId;
+    private String name;
+
+    //getter
+    public String getTeacherId() {
+        return this.teacherId;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    //setter
+    public void setTeacherId(String teacherId) {
+        this.teacherId = teacherId;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
