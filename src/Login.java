82e7feeef2b2faebfcd169aff984ee827c808e62
BeetlesTheHunter <jhon31708@gmail.com>
2026-02-06 14:22:09 +0900

    IDとパスワードのチェック追加

--- src/Login.java
+++ src/Login.java
@@ -1,18 +1,23 @@
 import java.util.Scanner;
-public class login {
+public class Login {
     
-    public static void loginScreen(){
+    public void loginScreen(){
         Scanner sc = new Scanner(System.in);
         System.out.println("IDを入力してください。");
         String userID = sc.nextLine();
         System.out.println("パスワードを入力してください。");
         String userPass = sc.nextLine();
         sc.close();//リソースの解放のためにあります。
+
+        if(checkUser(userID,userPass)){
+
+        }
     }
 
     private boolean checkUser(String _ID, String _pass){
 
-        
+        if(!_ID.equals("Testword")) return false;
+        if(!_ID.equals("Testword")) return false;
         return  true;
     }
 }
